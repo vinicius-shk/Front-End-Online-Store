d4119a1c8c5abf7c6d42ad504d8b73c7675784c1
David-Neto <davidrmachado@gmail.com>
2022-07-07 18:25:12 -0300

    Retirada da mensagem inicial

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ class Home extends Component {
   render() {
     return (
       <div data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
         <Categorias />
       </div>
     );

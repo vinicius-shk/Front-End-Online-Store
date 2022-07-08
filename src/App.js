06a5b6fc452a950a91cdc7bab6b54109d6813c79
caiosev <kaiosev9@gmail.com>
2022-07-08 16:06:20 -0300

    req 9

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Carrinho from './pages/Carrinho';
@@ -6,12 +6,23 @@ import Produto from './pages/Produto';
 import Home from './pages/Home';
 
 function App() {
+  const [itens, setItem] = useState([]);
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route path="/produto/:id" component={ Produto } />
-          <Route path="/carrinho" component={ Carrinho } />
+          <Route
+            path="/produto/:id"
+            render={ (props) => (<Produto
+              setItem={ setItem }
+              itens={ itens }
+              { ...props }
+            />) }
+          />
+          <Route
+            path="/carrinho"
+            render={ () => <Carrinho itens={ itens } /> }
+          />
           <Route exact path="/" component={ Home } />
         </Switch>
       </BrowserRouter>
--- src/pages/Carrinho.js
+++ src/pages/Carrinho.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Carrinho extends Component {
-  render() {
-    return (
-      <div
-        data-testid="shopping-cart-empty-message"
-      >
-        Seu carrinho está vazio
+  renderNullProduto = () => (
+    <div
+      data-testid="shopping-cart-empty-message"
+    >
+      Seu carrinho está vazio
+    </div>)
 
-      </div>
-    );
-  }
+    renderProds = () => {
+      const { itens } = this.props;
+      return itens.map((prod) => (
+        <div className="" key={ prod.id }>
+          <p data-testid="shopping-cart-product-name">{prod.title}</p>
+          <p>{prod.price}</p>
+          <p data-testid="shopping-cart-product-quantity">1</p>
+        </div>
+      ));
+    }
+
+    render() {
+      const { itens } = this.props;
+      return (
+        <div className="">
+          {itens.length === 0 && this.renderNullProduto()}
+          {itens.length > 0 && this.renderProds()}
+        </div>
+      );
+    }
 }
 
 export default Carrinho;
+
+Carrinho.propTypes = {
+  itens: PropTypes.array,
+}.isRequired;
